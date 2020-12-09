d9dcdb384b0e6154fe5376b564a89e884a00ed06
Amitoj Singh Saini <69375705+MRSTEALYOURCODE1@users.noreply.github.com>
2020-12-08 20:39:16 -0500

    Add files via upload

---
+++ OnOs3.0/animation/script.js
@@ -0,0 +1,8 @@
+console.log("Animation Found");
+window.addEventListener('online', () => console.log('came online'));
+window.addEventListener('offline', () => console.error('came offline'));
+
+
+function next() {
+    alert("Our Network Database Is Under Maintenance We Will Redirect You To The Next Part Of The Setup, Thank You.");
+}
